81a6f3791819308cd53ea4a274f1ee02ee86b85c
Aleksandar <aleksandar.gujas@gmail.com>
2019-03-29 22:40:17 +1000

    removed now

--- posts.js
+++ posts.js
@@ -5,21 +5,15 @@ const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(cors());
-
-const path = require('path');
-
-
-var corsOptions = {
-  origin: 'http://localhost:3000/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+app.options('*', cors());
 
 app.post('*', (req, res, next) => {
+    console.log('we are gettomg jere')
     const { firstName, lastName, phoneNumber, emailAddress, messageText } = req.body
 
     mailer.send( firstName, lastName, phoneNumber, emailAddress, messageText ).then(() => {
       console.log(`Sent the message "${messageText}" from <${firstName}> ${emailAddress}.`);
-      res.status(200);
+      res.status(200);  
       res.send('success')
     }).catch((error) => {
       console.log(`Failed to send the message "${messageText}" from <${firstName}> ${emailAddress} with the error ${error && error.message}`);
